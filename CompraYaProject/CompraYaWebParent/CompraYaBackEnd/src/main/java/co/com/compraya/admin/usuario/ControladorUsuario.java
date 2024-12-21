938bd1611b8a61b069b7c59cbe9276d7d34e7e0b
Jorge Cabrera <jorgecabrerasouto@outlook.com>
2024-12-21 12:57:50 -0500

    Implementada la función de ingresar un Usuario nuevo. Closes #6

--- CompraYaProject/CompraYaWebParent/CompraYaBackEnd/src/main/java/co/com/compraya/admin/usuario/ControladorUsuario.java
+++ CompraYaProject/CompraYaWebParent/CompraYaBackEnd/src/main/java/co/com/compraya/admin/usuario/ControladorUsuario.java
@@ -6,7 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import co.com.compraya.common.entity.Role;
 import co.com.compraya.common.entity.Usuario;
 
 @Controller
@@ -21,5 +24,28 @@ public class ControladorUsuario {
 		model.addAttribute("listaUsuarios", listaUsuarios);
 		return "usuarios";
 	}
-
+	
+	@GetMapping("/usuarios/nuevo")
+	
+	public String nuevoUsuario(Model model) {
+		List<Role> listaRoles = servicio.listaRoles();
+		
+		Usuario usuario = new Usuario();
+		usuario.setActivo(true);
+		model.addAttribute("usuario", usuario);
+		model.addAttribute("listaRoles", listaRoles);
+		
+		return "forma_usuario";
+	}
+	
+	@PostMapping("/usuarios/guardar")
+	public String guardarUsuario (Usuario usuario, RedirectAttributes redirectAttributes) {
+		System.out.println(usuario);
+		servicio.guardar(usuario);
+		
+		redirectAttributes.addFlashAttribute("message", "El usuario fue adicionado correctamente");
+		
+		return "redirect:/usuarios";
+	}
+	
 }
--- CompraYaProject/CompraYaWebParent/CompraYaBackEnd/src/main/java/co/com/compraya/admin/usuario/ServicioUsuario.java
+++ CompraYaProject/CompraYaWebParent/CompraYaBackEnd/src/main/java/co/com/compraya/admin/usuario/ServicioUsuario.java
@@ -5,16 +5,28 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import co.com.compraya.common.entity.Role;
 import co.com.compraya.common.entity.Usuario;
 
 @Service
 public class ServicioUsuario {
 	
 	@Autowired
-	private UsuarioRepository repo;
+	private UsuarioRepository usuarioRepo;
+
+	@Autowired
+	private RoleRepository roleRepo;	
 	
 	public List<Usuario> listAll() {
-		return (List<Usuario>) repo.findAll();
+		return (List<Usuario>) usuarioRepo.findAll();
 	}
 
+	public List<Role> listaRoles() {
+		return (List<Role>) roleRepo.findAll();
+		
+	}
+	
+	public void guardar(Usuario usuario) {
+		usuarioRepo.save(usuario);
+	}
 }
