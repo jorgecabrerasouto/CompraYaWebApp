15d605e8993a38b08569dc74228ae99fec118801
Jorge Cabrera <jorgecabrerasouto@outlook.com>
2026-01-21 11:14:45 -0500

    Se realizó la Refactorización de Código #5. Se simplificó la configuración realizada en MvcConfig.java. Closes #59

--- CompraYaProject/CompraYaWebParent/CompraYaBackEnd/src/main/java/co/com/compraya/admin/MvcConfig.java
+++ CompraYaProject/CompraYaWebParent/CompraYaBackEnd/src/main/java/co/com/compraya/admin/MvcConfig.java
@@ -12,28 +12,20 @@ public class MvcConfig implements WebMvcConfigurer {
 
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		String nombreDirectorio = "foto-usuarios";
-		Path trayectoriaDirectorioFotos = Paths.get(nombreDirectorio);
 		
-		String trayectoriaFotoUsuarios = trayectoriaDirectorioFotos.toFile().getAbsolutePath();
-		
-		registry.addResourceHandler("/" + nombreDirectorio + "/**")
-			.addResourceLocations("file:" + trayectoriaFotoUsuarios + "/");
-		
-		String nombreDirImagenesCategorias = "../imagenes-categorias";
-		Path trayectoriaDirectorioImagenes = Paths.get(nombreDirImagenesCategorias);
-		
-		String trayectoriaImagenesCategoria = trayectoriaDirectorioImagenes.toFile().getAbsolutePath();
-		
-		registry.addResourceHandler("/imagenes-categorias" + "/**")
-			.addResourceLocations("file:" + trayectoriaImagenesCategoria + "/");
-	
-		String nombreDirLogosMarca = "../logos-marca";
-		Path trayectoriaDirectorioLogos = Paths.get(nombreDirLogosMarca);
-	
-		String trayectoriaLogosMarca = trayectoriaDirectorioLogos.toFile().getAbsolutePath();
-	
-		registry.addResourceHandler("/logos-marca/**")
-			.addResourceLocations("file:/" + trayectoriaLogosMarca + "/");	
+		exponerDirectorio("foto-usuarios", registry);
+		exponerDirectorio("../imagenes-categorias", registry);
+		exponerDirectorio("../logos-marca", registry);		
 	}
+		
+		private void exponerDirectorio(String patronTrayectoria, ResourceHandlerRegistry registry) {
+			Path trayectoria = Paths.get(patronTrayectoria);
+			String trayectoriaAbsoluta = trayectoria.toFile().getAbsolutePath();
+			
+			String trayectoriaRelativa = patronTrayectoria.replace("..", "") + "/**";
+								
+			registry.addResourceHandler(trayectoriaRelativa)
+			.addResourceLocations("file:/" + trayectoriaAbsoluta + "/");
+	}
+	
 }
